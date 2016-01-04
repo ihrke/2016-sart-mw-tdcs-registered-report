3297a1ab5459d3b725d2fd3d735a544357ff9ac4
Matthias Mittner <matthias.mittner@uit.no>
2016-01-04 11:33:02 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 2016-sart-mw-tdcs-registered-report
-This repo hosts code for stimulus presentation for the registered report in: Mittner, M. (2016, January 4). Registered Report: Increasing/Decreasing propensity of mind wandering using tDCS. Retrieved from osf.io/dct2r
+# Sustained Attention to Response Task (SART)
+
+This repo hosts code for stimulus presentation for the registered report in: Mittner, M. (2016, January 4). Registered Report: Increasing/Decreasing propensity of mind wandering using tDCS. Retrieved from <osf.io/dct2r>.
+
+Currently, English and Norwegian translations are available.
+
+Code is based on PsychoPy.
---
+++ sart/instructions_en.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+instructions={}
+instructions['welcome']=[]
+instructions['welcome'].append(
+    ["Welcome and thank you very much for participating in this study.",
+#     "Please adjust your seat and body to a comfortable position"\
+#     "before continuing by pressing one of the two buttons.",
+     "press button to continue"])
+
+instructions['welcome'].append(
+    ["In this experiment, you will see the numbers between 1 and 9 "\
+     "in the center of the screen.",
+     "Your task is to press a button in response to each of these "\
+     "numbers EXCEPT to the number 3. Whenever you see a 3 on "\
+     "the screen, you should NOT press a button.",
+     "Please try to respond as fast as possible to all numbers "\
+     "except 3 but try also to be as accurate as possible "\
+     "in withholding the response to the number 3.",
+     "press button to continue"])
+
+instructions['welcome'].append(
+    ["In addition, you will every once in a while be presented with "\
+     "a screen asking about your current thoughts.",
+     "You will have to rate on a scale from 1 to 4 whether or not "\
+     "you were fully concentrated on the task. ",
+     "If you were fully concentrated, you would respond with a "\
+     "number at the high end of the scale (4) which is labeled "
+     "\"on-task\".",
+     "The lower end of the scale (1) which is labeled "\
+     "\"off-task\" refers to both to thoughts that are ",
+     "* completely unrelated to the experiment (e.g., you were "\
+     "  thinking about your studies or a book you've read)",
+     "* or related the the experiment but not about the immediate "\
+     "task (e.g., you were evaluating yourself in terms of "\
+     "whether you make a lot of errors)",
+     "press button to continue"])
+
+
+instructions['welcome'].append(
+    ["On the following screen, you will see an example of such a "\
+     "question. Please adjust the arrow pointing on each possible "\
+     "number until it matches your current thinking.",
+     "The screen disappears automatically after 6 seconds. Please "\
+     "take care that the arrow points to the correct number by "\
+     "then.",
+     "press button to continue"])
+
+
+
+instructions['probetraining']=[]
+instructions['probetraining'].append(
+    ["In case you missed it, here is another one.",
+    "press button to continue"])
+
+instructions['probetraining'].append(
+    ["Do you want to see another one?.",
+    "Press left for another try, right to continue."])
+
+instructions['training']=[]
+instructions['training'].append(
+    ["You are now ready to start a practise version of the experiment.",
+     "Please remember to press the button as fast as possible to all "\
+     "numbers except the number 3.",
+     "If you have any question, please contact the experimenter "\
+     "before continuing.",
+     "press button to continue"])
+
+instructions['experiment']=[]
+instructions['experiment'].append(
+    ["The practice session is over and the real experiment is about "\
+     "to start. Please contact the experimenter in case you have "\
+     "any questions left.",
+     "press button to continue"])
+
+instructions['experiment'].append(
+    ["The experiment starts as soon as you press the button.",
+#     "Please sit comfortably as the experiment will take some time. "\
+     "After half of the experiment is over, you can take a short "\
+     "break.",
+     "Please remember to press the button as fast as possible to all "\
+     "numbers except the number 3.",
+     "press button to continue"])
+
+
+instructions['halfway']=[]
+instructions['halfway'].append(
+    ["You are halfway through the experiment, thank you for your "\
+     "cooperation.",
+     "Please relax a bit before continuing the "\
+     "experiment. ",
+     "press button to continue"])
+
+instructions['halfway'].append(
+    ["The second half of the experiment starts as soon as you press "\
+     "the button.",
+     "Please remember to press the button as fast as possible to all "\
+     "numbers except the number 3.",
+     "press button to continue"])
+
+instructions['finish']=[]
+instructions['finish'].append(
+    ["The experiment is now finished.",
+     "Thank you very much for taking part in this study. "])
